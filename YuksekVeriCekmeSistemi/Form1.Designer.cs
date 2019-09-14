dfd9dfe44ca1d910ce20cf3283754b68e56f9850
ebubekirbastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-09-15 01:15:15 +0300

    Add project files.

---
+++ YuksekVeriCekmeSistemi/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace YuksekVeriCekmeSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(480, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 240);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SyhMhfz Form Donma sorunu Sözme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ YuksekVeriCekmeSistemi/syhmhfzkntlmrkz.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YuksekVeriCekmeSistemi
+{
+    public class syhmhfzkntlmrkz
+    {
+        int veri = 0;
+        public Task securtylogi(ListBox lst)
+        {
+            return Task.Run(() =>
+            {
+                islembaslat(lst);
+            });
+        }
+        void  islembaslat(ListBox lst)
+        {
+            for (int i = 0; i < 999999999999999; i++)
+            {
+                lst.Items.Add(veri);
+                veri++;
+            }
+        }
+    }
+}
